38b7edf58e5721755518f747ac63c0a542e7a004
Matias Garcia <matiasagarcia8@gmail.com>
2022-03-08 21:37:25 -0300

    fixing details

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "./CartContext";
 import { Link } from "react-router-dom";
 import {
@@ -19,10 +19,6 @@ const Cart = () => {
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
-
-	useEffect(() => {
-		orderId !== null && handleShow();
-	}, []);
 
 	const createOrder = () => {
 		let order = {
@@ -45,10 +41,9 @@ const Cart = () => {
 		};
 		createOrderInFirestore()
 			.then((res) => {
-				//CAMBIAR POR MODAL
 				setOrderId(res.id);
-				alert("Your order has been created: " + res.id);
 			})
+			.then(handleShow())
 			.then(
 				cartData.cartList.map(async (item) => {
 					const itemRef = doc(db, "products", item.id);
@@ -62,6 +57,82 @@ const Cart = () => {
 	};
 
 	return cartData.cartList.length > 0 ? (
+		<div className='container'>
+			<div className='container'>
+				<div className='row cartTitles'>
+					<div className='col-9'>
+						<h2>Tu Carrito</h2>
+					</div>
+					<div className='col-3'>
+						<p>
+							Total de Artículos: <span>{cartData.cartList.length}</span>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className='container'>
+				<div className='row cartRow cartHeader'>
+					<div className='col-4'>Productos</div>
+					<div className='col-3'>Cantidad</div>
+					<div className='col-3'>Precio Unitario</div>
+					<div className='col'>Total</div>
+				</div>
+			</div>
+
+			{cartData.cartList.map((item) => (
+				<div className='container'>
+					<div className='row cartRow '>
+						<div className='col-2'>
+							<img className='cartImg' src={item.smallPic} />
+						</div>
+						<div className='col-2'>
+							<p>{item.title}</p>
+							<button
+								className='btn btn-danger'
+								onClick={() => cartData.deleteProduct(item.id)}>
+								Eliminar
+							</button>
+						</div>
+
+						<div className='col-3'>{item.qty}</div>
+						<div className='col-3'>${item.price}</div>
+						<div className='col'>${item.qty * item.price}</div>
+					</div>
+				</div>
+			))}
+			<div className='container total'></div>
+			<div className='container'>
+				<div className='row cartRow'>
+					<div className='col-7'></div>
+					<div className='col-3'>
+						<p style={{ fontWeight: "bold" }}>Total: </p>
+					</div>
+					<div className='col'>
+						<p style={{ fontWeight: "bold" }}> ${cartData.totalCost}</p>
+					</div>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='row cartRow'>
+					<div className='col-6'>
+						<Link to='/'>
+							<p className='continueShopping'>
+								Seguí eligiendo tu mejor deco...
+							</p>
+						</Link>
+					</div>
+					<div className='col-6 btnComprarContainer '>
+						<button
+							onClick={createOrder}
+							className='btn btn-success btnComprar'>
+							Comprar
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	) : (
 		<>
 			<Modal
 				show={show}
@@ -80,91 +151,14 @@ const Cart = () => {
 					</Button>
 				</Modal.Footer>
 			</Modal>
-			<div className='container'>
-				<div className='container'>
-					<div className='row cartTitles'>
-						<div className='col-9'>
-							<h2>Tu Carrito</h2>
-						</div>
-						<div className='col-3'>
-							<p>
-								Total de Artículos: <span>{cartData.cartList.length}</span>
-							</p>
-						</div>
-					</div>
-				</div>
 
-				<div className='container'>
-					<div className='row cartRow cartHeader'>
-						<div className='col-4'>Productos</div>
-						<div className='col-3'>Cantidad</div>
-						<div className='col-3'>Precio Unitario</div>
-						<div className='col'>Total</div>
-					</div>
-				</div>
-
-				{cartData.cartList.map((item) => (
-					<div className='container'>
-						<div className='row cartRow '>
-							<div className='col-2'>
-								<img className='cartImg' src={item.smallPic} />
-							</div>
-							<div className='col-2'>
-								<p>{item.title}</p>
-								<button
-									className='btn btn-danger'
-									onClick={() => cartData.deleteProduct(item.id)}>
-									Eliminar
-								</button>
-							</div>
-
-							<div className='col-3'>{item.qty}</div>
-							<div className='col-3'>${item.price}</div>
-							<div className='col'>${item.qty * item.price}</div>
-						</div>
-					</div>
-				))}
-				<div className='container total'></div>
-				<div className='container'>
-					<div className='row cartRow'>
-						<div className='col-7'></div>
-						<div className='col-3'>
-							<p style={{ fontWeight: "bold" }}>Total: </p>
-						</div>
-						<div className='col'>
-							<p>${cartData.totalCost}</p>
-						</div>
-					</div>
-				</div>
-				<div className='container'>
-					<div className='row cartRow'>
-						<div className='col-6'>
-							<Link to='/'>
-								<p className='continueShopping'>
-									Seguí eligiendo tu mejor deco...
-								</p>
-							</Link>
-						</div>
-						<div className='col-6 btnComprarContainer '>
-							<button
-								onClick={() => {
-									createOrder();
-								}}
-								className='btn btn-success btnComprar'>
-								Comprar
-							</button>
-						</div>
-					</div>
-				</div>
+			<div className='container carroVacio'>
+				<h2>Aún no has agregado nada al carrito!</h2>
+				<Link to='/'>
+					<p className='continueShopping'>Seguí eligiendo tu mejor deco...</p>
+				</Link>
 			</div>
 		</>
-	) : (
-		<div className='container carroVacio'>
-			<h2>Aún no has agregado nada al carrito!</h2>
-			<Link to='/'>
-				<p className='continueShopping'>Seguí eligiendo tu mejor deco...</p>
-			</Link>
-		</div>
 	);
 };
 
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,47 +2,69 @@ import ItemCount from "./ItemCount";
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
+import { Modal, Button } from "react-bootstrap";
 
 const ItemDetail = ({ item }) => {
 	const [itemCount, setItemCount] = useState(0);
 	const globalCart = useContext(CartContext);
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	const onAdd = (qty) => {
 		//CAMBIAR POR MODAL
-		alert("Se agregaron al carrito " + qty + " unidades");
+		handleShow();
 		setItemCount(qty);
 		//Funcion global del Context
 		globalCart.addToCart({ ...item, qty }, qty);
 	};
 
 	return (
-		<div className='container detailContainer'>
-			<div className='row'>
-				<div className='col-3'>
-					<img
-						className='card-img-top'
-						src={item.pictureUrl}
-						alt={item.title}
-					/>
+		<>
+			<Modal
+				show={show}
+				onHide={handleClose}
+				backdrop='static'
+				keyboard={false}>
+				<Modal.Header closeButton>
+					<Modal.Title>Agregado al carrito</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Se agregaron al carrito {itemCount} unidades</Modal.Body>
+				<Modal.Footer>
+					<Button variant='primary' onClick={handleClose}>
+						Aceptar
+					</Button>
+				</Modal.Footer>
+			</Modal>
+			<div className='container detailContainer'>
+				<div className='row'>
+					<div className='col-3'>
+						<img
+							className='card-img-top'
+							src={item.pictureUrl}
+							alt={item.title}
+						/>
+					</div>
+					<div className='col-9'>
+						<h2>{item.title}</h2>
+						<h4 className='card-text'>{item.description}</h4>
+						<p>{item.details}</p>
+						<p>Stock disponible: {item.stock}</p>
+						<p className='detailPrice'>${item.price}</p>
+					</div>
 				</div>
-				<div className='col-9'>
-					<h2>{item.title}</h2>
-					<h4 className='card-text'>{item.description}</h4>
-					<p>{item.details}</p>
-					<p>Stock disponible: {item.stock}</p>
-					<p className='detailPrice'>${item.price}</p>
-				</div>
+				{itemCount === 0 ? (
+					<ItemCount stock={item.stock} initial={ItemCount} onAdd={onAdd} />
+				) : (
+					<Link to='/cart'>
+						<button className='btn btn-success btn-checkout'>
+							Finalizar Compra
+						</button>
+					</Link>
+				)}
 			</div>
-			{itemCount === 0 ? (
-				<ItemCount stock={item.stock} initial={ItemCount} onAdd={onAdd} />
-			) : (
-				<Link to='/cart'>
-					<button className='btn btn-success btn-checkout'>
-						Finalizar Compra
-					</button>
-				</Link>
-			)}
-		</div>
+		</>
 	);
 };
 
